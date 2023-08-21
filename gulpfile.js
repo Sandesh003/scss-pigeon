4cab2fc5ab4fc4797bbffd20d5cad07ba162c066
Sandesh Ghone <sandesh.fluxbyte@gmail.com>
2023-08-21 12:06:02 +0530

    rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
-const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'));
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src('pegon/**/*.scss')
-    .pipe(sass())
-    .pipe(dest('css'))
+  return src("pigeon/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(['pegon/**/*.scss'], buildStyles)
+  watch(["pigeon/**/*.scss"], buildStyles);
 }
 
-exports.default = series(buildStyles, watchTask)
+exports.default = series(buildStyles, watchTask);
